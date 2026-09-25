150c5272c014ccc6c30057e449cfd89290c0625d
Jeronimo <jeromendezbetancur10@gmail.com>
2026-09-25 11:40:43 -0500

    test: break resource reference to verify CI catches it

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "main" {
+resource "azurerm_resource_group" "mian" {
   name     = "rg-${local.name_suffix}"
   location = var.location
   tags     = local.common_tags
